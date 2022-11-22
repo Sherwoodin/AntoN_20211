a1002bc2eff7296846929c920bfa325dc7cd7afa
kot3u <kot3uko_1337@mail.ru>
2022-11-22 19:25:06 +0300

    всё

---
+++ AntoN_20211/project/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path("", views.project_index, name="project_index"),
+    path("<int:pk>/", views.project_detail, name="project_index"),
+
+    ]
