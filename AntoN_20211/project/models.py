6bd972c4ae1bb934e1a1f98bae991d03f9475fde
kot3u <kot3uko_1337@mail.ru>
2022-11-25 13:15:30 +0300

    всё

--- AntoN_20211/project/models.py
+++ AntoN_20211/project/models.py
@@ -7,3 +7,19 @@ class Project(models.Model):
     technology = models.CharField(max_length=50)
     image = models.FileField(upload_to='img/')
 
+class Category(models.Model):
+    name = models.CharField(max_length=30)
+
+class Post(models.Model):
+    title = models.CharField(max_length=250)
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True)
+    categories = models.ManyToManyField('Category', related_name='posts')
+
+class Comment(models.Model):
+    author = models.CharField(max_length=60)
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    post = models.ForeignKey('Post', on_delete=models.CASCADE)
+
--- AntoN_20211/project/urls.py
+++ AntoN_20211/project/urls.py
@@ -5,6 +5,6 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", views.project_index, name="project_index"),
-    path("<int:pk>/", views.project_detail, name="project_detail    "),
+    path("<int:pk>/", views.project_detail, name="project_detail"),
 
     ]
